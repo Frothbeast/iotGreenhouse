667bc671174323e9ad29aa2b78cdd0a012eb5628
Frothbeast <frothbeast@gmail.com>
2026-03-20 00:34:50 -0400

    hours
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- server/greenhouseAPI.py
+++ server/greenhouseAPI.py
@@ -91,7 +91,10 @@ def get_data():
         print(f"DEBUG: Fetching data for last {hours} hours", file=sys.stderr, flush=True)
         conn = get_db_connection()
         cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT datetime, tempHigh, tempLow, rssiHigh, rssiLow, readingCount, notes FROM greenhouseData WHERE datetime > NOW() - INTERVAL %s HOUR")
+        # cursor.execute("SELECT datetime, tempHigh, tempLow, rssiHigh, rssiLow, readingCount, notes FROM greenhouseData WHERE datetime > NOW() - INTERVAL %s HOUR")
+        cursor.execute(
+            "SELECT datetime, tempHigh, tempLow, rssiHigh, rssiLow, readingCount, notes FROM greenhouseData WHERE datetime > NOW() - INTERVAL %s HOUR",
+            (hours,))
         rows = cursor.fetchall()
         cursor.close()
         conn.close()
