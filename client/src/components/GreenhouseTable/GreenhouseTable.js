a39b8a582026f2c5310194eccd65f92a58de6471
Frothbeast <frothbeast@gmail.com>
2026-03-20 21:43:07 -0400

    greenrecords is just records
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- client/src/components/GreenhouseTable/GreenhouseTable.js
+++ client/src/components/GreenhouseTable/GreenhouseTable.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './GreenhouseTable.css';
 
-const GreenhouseTable = ({ greenrecords = [], columnStats }) => {
+const GreenhouseTable = ({ records = [], columnStats }) => {
     if (!columnStats) return null;
 
     return (
