4f3036d86912966911cff475d330a4d7fa479de7
frothbeast <frothbeast@gmail.com>
2026-03-25 12:57:44 -0400

    _ is no -

--- server/start.sh
+++ server/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-docker network inspect iot_network >/dev/null 2>&1 || \
-    docker network create --driver bridge iot_network
-
-
 python greenhouseCollector.py &
 python greenhouseAPI.py
