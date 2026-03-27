5426d4ab882146144cc48bceb2af81bff1641e4a
Frothbeast <frothbeast@gmail.com>
2026-03-26 21:19:05 -0400

    adding firmware for small dish unit
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

---
+++ ESP/smaller dish/configExample.h
@@ -0,0 +1,12 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* WIFI_SSID = "";
+const char* WIFI_PASS = "";
+const char* SERVER_URL = "http://192.168.xxx.xxx:5000/greenhouse";
+const int SERVER_PORT = the esp port number
+const char* DEVICE_ID = name for the esp unit
+const char* SERVER_IP = "";
+
+#endif
